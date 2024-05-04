94629c0dc5c8e2d188c758ba7bbce9b9a8550ff7
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-05-04 23:49:32 +0300

    add JSON data
    Store and load data from JSON, handle exceptions

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ logging.debug("Bot token: %s", BOT_TOKEN)
 logging.debug("Log level: %d", LOG_LEVEL)
 
 
+def read_json(json_filename: str) -> dict:
+    try:
+        with open(json_filename, mode="r") as f:
+            result = json.load(f)
+
+        if not isinstance(result, dict):
+            raise Exception("Expected result to be dict")
+
+        return result
+    except OSError as e:
+        logging.error("Error reading JSON file %s", json_filename, exc_info=e)
+    except Exception as e:
+        logging.critical("Uncaught exception while reading JSON file %", json_filename, exc_info=e)
+
+
+data = read_json("data.json")
+captions = read_json("captions.json")
+
+
 try:
     logging.info("Configuring the bot...")
     bot = telebot.TeleBot(BOT_TOKEN)
