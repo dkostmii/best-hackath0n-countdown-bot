739f14a8eaba2b4f7a5b2f1e91c2f7486a637fa8
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-05-11 06:58:12 +0300

    Fix: pluralization
    Fix 11 is not properly pluralized, use `"one"` as a fallback, pass `n=1`
    to it.

--- pluralize.py
+++ pluralize.py
@@ -1,18 +1,24 @@
 def pluralize(n: int = 1, pluralizations_dict: dict[str, str] = {}, **kwargs) -> str:
     if n == 0:
+        if pluralizations_dict["zero"] is None:
+            return pluralizations_dict["one"].format(n=1, **kwargs)
+
         return pluralizations_dict["zero"].format(n=n, **kwargs)
 
     last_digit = n % 10
     is_nteen = 10 <= n <= 19
 
-    if last_digit == 1:
+    if last_digit == 1 and not is_nteen:
         return pluralizations_dict["one"].format(n=n, **kwargs)
     
     if 2 <= last_digit <= 4 and not is_nteen:
         if pluralizations_dict["few"] is None:
-            return pluralizations_dict["one"].format(n=n, **kwargs)
+            return pluralizations_dict["one"].format(n=1, **kwargs)
         
         return pluralizations_dict["few"].format(n=n, **kwargs)
+
+    if pluralizations_dict["many"] is None:
+        return pluralizations_dict["one"].format(n=1, **kwargs)
 
     return pluralizations_dict["many"].format(n=n, **kwargs)
 
